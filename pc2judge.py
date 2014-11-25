a07300a19a2bd007947ab8b0aab3029534975e6c
bruce791130 <bruce791130@users.noreply.github.com>
2014-11-25 19:36:14 +0800

    Update pc2judge.py

--- pc2judge.py
+++ pc2judge.py
@@ -5,6 +5,16 @@ import cgi
 import socket
 import sys
 from urlparse import urlparse
+from lxml import etree
+from pkg_resources import resource_string
+
+from xmodule.x_module import XModule
+from xmodule.raw_module import RawDescriptor
+from xblock.core import Scope, String
+from xmodule.annotator_mixin import get_instructions
+from xmodule.annotator_token import retrieve_token
+from xblock.fragment import Fragment
+import textwrap
 
 from xblock.core import XBlock
 from xblock.fields import Boolean, DateTime, Scope, String, Float, Integer
