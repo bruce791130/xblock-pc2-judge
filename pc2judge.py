795ef4413457c092b00d2f5e5c5e4704126bb970
bruce791130 <bruce791130@users.noreply.github.com>
2014-11-25 22:23:58 +0800

    Update pc2judge.py

--- pc2judge.py
+++ pc2judge.py
@@ -83,7 +83,8 @@ class Pc2JudgeBlock(XBlock):
            
             self.score_published2 = True
             self.score_approved2 = True
-        
+            
+        self.runtime.publish('progress', (10, self.max_score()))
         html_str = pkg_resources.resource_string(__name__, "static/html/Pc2Judge.html")
         frag = Fragment(unicode(html_str).format(self=self))
         frag.add_css("""
