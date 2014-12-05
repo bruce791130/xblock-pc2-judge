5b67eabfafe010347e95e75dba414c25017306b1
bruce791130 <bruce791130@users.noreply.github.com>
2014-12-05 18:37:08 +0800

    Create Pc2Judge_3.js

---
+++ static/js/Pc2Judge_3.js
@@ -0,0 +1,12 @@
+function Pc2JudgeBlock(runtime, element) {
+    
+ var v =  document.getElementById("edx").value;
+ var v2 =  document.getElementById("edx2").value;
+document.getElementById("iframe1").src = "http://acnlab.openedx.tw/error.html?id="+v+"&problem="+v2;
+	   
+//location.href ="http://acnlab.openedx.tw/pc2.html?id="+"ffff"; 
+//window.location="http://www.w3schools.com/"+"js/js_input_examples.asp"
+
+
+    
+}
