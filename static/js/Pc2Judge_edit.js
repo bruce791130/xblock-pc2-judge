8e0a28a00c06d864c2f33ad2dd53dc1d1c09c856
bruce791130 <bruce791130@users.noreply.github.com>
2014-12-07 21:06:06 +0800

    Update Pc2Judge_edit.js

--- static/js/Pc2Judge_edit.js
+++ static/js/Pc2Judge_edit.js
@@ -1,10 +1,12 @@
 function Pc2JudgeEditBlock(runtime, element) {
   $(element).find('.save-button').bind('click', function() {
     var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
-    var edxdata = {
-      problemtext: $(element).find('input[name=problemtext]').val(),
+    var data = {
+      
+      maxwidth: $(element).find('input[name=maxwidth]').val(),
+      maxheight: $(element).find('input[name=maxheight]').val()
     };
-    $.post(handlerUrl, JSON.stringify(edxdata)).done(function(response) {
+    $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
       window.location.reload(false);
     });
   });
