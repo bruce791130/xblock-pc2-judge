ec02b858c351a07d27ac0d9ce196b2153ae2c7ac
bruce791130 <bruce791130@users.noreply.github.com>
2014-12-07 17:50:54 +0800

    Create Pc2Judge_edit.js

---
+++ static/js/Pc2Judge_edit.js
@@ -0,0 +1,15 @@
+function Pc2JudgeEditBlock(runtime, element) {
+  $(element).find('.save-button').bind('click', function() {
+    var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
+    var data = {
+      href: $(element).find('input[name=href]').val(),
+    };
+    $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
+      window.location.reload(false);
+    });
+  });
+
+  $(element).find('.cancel-button').bind('click', function() {
+    runtime.notify('cancel', {});
+  });
+}
