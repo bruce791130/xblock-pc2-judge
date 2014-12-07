36f6b6efcbe0c355e3c104435c34da2c2e3ec2d0
bruce791130 <bruce791130@users.noreply.github.com>
2014-12-07 19:16:16 +0800

    Update Pc2Judge_edit.js

--- static/js/Pc2Judge_edit.js
+++ static/js/Pc2Judge_edit.js
@@ -2,7 +2,7 @@ function Pc2JudgeEditBlock(runtime, element) {
   $(element).find('.save-button').bind('click', function() {
     var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
     var data = {
-      href: $(element).find('input[name=problemtext]').val(),
+      problemtext: $(element).find('input[name=problemtext]').val(),
     };
     $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
       window.location.reload(false);
